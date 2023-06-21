b662c514ef4fb34d0199639c9844fdb0f6774f63
Navid Sassan <navid.sassan@linuxfabrik.ch>
2023-06-21 13:22:16 +0200

    use requirement.txt

--- build/shared/venv.sh
+++ build/shared/venv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+
 python3 -m venv --system-site-packages pyinstaller
 source pyinstaller/bin/activate
 
@@ -9,25 +9,5 @@ pip install --upgrade wheel
 pip install --upgrade setuptools
 pip install pyinstaller
 
-# install any libraries specific for the project:
-pip install argparse
-pip install beautifulsoup4
-pip install certifi
-pip install cffi
-pip install colorama
-pip install counter
-pip install datetime
-pip install jinja2
-pip install lxml
-pip install netifaces
-pip install path
-pip install psutil
-pip install pymysql
-pip install pysmbclient
-pip install pyspnego
-pip install python-keystoneclient
-pip install python-swiftclient
-pip install smbprotocol
-pip install uuid
-pip install vici
-pip install xmltodict
+# install any libraries specific for the project
+pip install --requirement ../../requirements.txt
