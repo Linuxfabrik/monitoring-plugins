f28371261640109b23c442fb035edf178f8c7694
Navid Sassan <navid.sassan@linuxfabrik.ch>
2024-05-31 12:42:44 +0200

    ci/cd: add build_options

--- build/shared/compile.sh
+++ build/shared/compile.sh
@@ -6,7 +6,12 @@ for dir in /repos/monitoring-plugins/check-plugins/*; do
     check="$(basename $dir)"
     if [ "$check" != "example" ]; then
         echo -e "\ncompiling $check..."
-        pyinstaller --clean --distpath /tmp/dist/check-plugins --workpath /tmp/build/check-plugins --specpath /tmp/spec/check-plugins --noconfirm --noupx --onedir "$dir/${check}"
+        pyinstaller_extra_cmdline=''
+        if [ -f "$dir/.build_options" ]; then
+            echo "Found .build_options, sourcing them"
+            . "$dir/.build_options"
+        fi
+        pyinstaller --clean --distpath /tmp/dist/check-plugins --workpath /tmp/build/check-plugins --specpath /tmp/spec/check-plugins --noconfirm --noupx --onedir $pyinstaller_extra_cmdline "$dir/${check}"
     fi
 done
 \cp -a --no-clobber /tmp/dist/check-plugins/*/* /tmp/dist/summary/check-plugins
