9709e7e269ebd079b031be4e31aebf99b382ad6b
Markus Frei <markus.frei@linuxfabrik.ch>
2025-02-20 15:48:13 +0100

    chore(get-latest-version.sh): implement github api response pagination

--- build/get-latest-version.sh
+++ build/get-latest-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# 2025021101
+# 2025022001
 
 set -e -x
 
@@ -8,15 +8,39 @@ if ! command -v jq &> /dev/null; then
     sudo apt -y install jq
 fi
 
-echo "Fetching all artifacts via REST and filter by name to get the latest version"
+echo "✅ Fetching all artifacts via REST and filtering by name to get the latest version"
 ARTIFACT_NAME="version"
-ARTIFACT_INFO=$(curl --silent \
-    --header "Authorization: Bearer $GITHUB_TOKEN" \
-    --header "Accept: application/vnd.github.v3+json" \
-    "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/actions/artifacts" | \
-    jq --raw-output ".artifacts | map(select(.name == \"$ARTIFACT_NAME\")) | sort_by(.created_at) | last // {}")
+PAGE=1
+ARTIFACT_INFO="{}"
 
-# Extract the download URL
+while true; do
+    echo "✅ Fetching page $PAGE..."
+    RESPONSE=$(curl --silent \
+      --header "Authorization: Bearer $GITHUB_TOKEN" \
+      --header "Accept: application/vnd.github.v3+json" \
+      "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/actions/artifacts?per_page=100&page=$PAGE")
+
+    # Check if there are any artifacts on this page.
+    ARTIFACT_COUNT=$(echo "$RESPONSE" | jq ".artifacts | length")
+    if [[ "$ARTIFACT_COUNT" -eq 0 ]]; then
+        echo "❌ No more artifacts found. Exiting..."
+        exit 1
+    fi
+
+    # Filter for artifacts with the name "version" and get the one with the latest created_at.
+    ARTIFACT_INFO=$(echo "$RESPONSE" | jq --raw-output \
+        ".artifacts | map(select(.name == \"$ARTIFACT_NAME\")) | sort_by(.created_at) | last // {}")
+
+    # If we found an artifact, break out of the loop.
+    if [[ "$ARTIFACT_INFO" != "{}" ]]; then
+        echo "✅ Found $ARTIFACT_NAME artifact on page $PAGE."
+        break
+    fi
+
+    PAGE=$((PAGE+1))
+done
+
+# Extract the download URL.
 ARTIFACT_URL=$(echo "$ARTIFACT_INFO" | jq --raw-output ".archive_download_url // empty")
 
 if [[ -z "$ARTIFACT_URL" ]]; then
@@ -24,7 +48,8 @@ if [[ -z "$ARTIFACT_URL" ]]; then
     exit 1
 else
     echo "✅ Downloading artifact from: $ARTIFACT_URL"
-    curl --header "Authorization: Bearer $GITHUB_TOKEN" \
+    curl --silent \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
         --location \
         --output version.zip \
         "$ARTIFACT_URL"
@@ -34,7 +59,7 @@ else
     echo "✅ Extracted version: $LFMP_VERSION"
     # Append an environment variable for later steps
     if [[ -z $GITHUB_ENV ]]; then
-        # we are not on an GitHub runner
+        # we are not on a GitHub runner
         export LFMP_VERSION=$LFMP_VERSION
     else
         echo "LFMP_VERSION=$LFMP_VERSION" >> "$GITHUB_ENV"
